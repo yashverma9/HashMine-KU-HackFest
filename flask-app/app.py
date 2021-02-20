7ed942f8e04a8409faab05d84c9ba13621a3bb87
androdat <saxenavedant61@gmail.com>
2021-02-20 19:14:22 +0530

    camera module working

--- flask-app/app.py
+++ flask-app/app.py
@@ -6,12 +6,16 @@ app.static_folder = 'static'
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 
 @app.route("/camera")
 def camera():
     return render_template("camera.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
